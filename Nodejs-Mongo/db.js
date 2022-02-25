83093b4b053235be1b99a4f424cf7e7c35130cae
Serena Sensini <serena.sensini@gmail.com>
2022-02-25 13:03:21 +0100

    Nodejs + Mongo fix

--- Nodejs-Mongo/db.js
+++ Nodejs-Mongo/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'utente';
+const MONGO_USERNAME = 'myuser';
 const MONGO_PASSWORD = 'password';
-const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_HOSTNAME = 'db';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'fruits';
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
+console.log("#####################");
+console.log(url);
+console.log("#####################");
 mongoose.connect(url, {useNewUrlParser: true});
